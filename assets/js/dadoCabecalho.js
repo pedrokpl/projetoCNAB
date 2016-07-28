f0e730aa9b26d3e1a8f71b9c7f72babf53174248
Pedro Goncalves <pedro.goncalves@KPL.LOCAL>
2016-07-28 09:13:01 -0300

    CRUD dos dados do cabecalho
    Adicionadas as funções de CRUD das dos cabeçalhos da formatação.

--- assets/js/dadoCabecalho.js
+++ assets/js/dadoCabecalho.js
@@ -1,6 +1,7 @@
 'use strict';
 
 app.controller('DadoCabecalhoController',['$scope', '$rootScope', '$http', '$location', '$routeParams', function($scope, $rootScope, $http, $location, $routeParams){
+	$scope.thisDadoCabecalho = this;
 	this.dadoCabecalho = {};
 	this.dadoCabecalho.formatacao = $routeParams.id;
 	$scope.dadosCabecalho = [];
@@ -10,18 +11,23 @@ app.controller('DadoCabecalhoController',['$scope', '$rootScope', '$http', '$loc
 	});
 
 	this.criar = function(){
-		$http.post('/dadoCabecalho/create', this.dadoCabecalho).success(function(resp){
-			console.log('Criado!');
-			$http.get('/formatacao/' + $routeParams.id).success(function(resp){
-				$scope.dadosCabecalho = resp.dadosCabecalho;
+		if($scope.thisDadoCabecalho.dadoCabecalho.id == null){
+			$http.post('/dadoCabecalho/create', this.dadoCabecalho).success(function(resp){
+				console.log('Criado!');
 			});
-		});
+		}else{
+			$http.put('/dadoCabecalho/' + this.dadoCabecalho.id, this.dadoCabecalho).success(function(resp){
+				console.log('Editado!');
+			});
+		}
+		$scope.dadosCabecalho.push($scope.thisDadoCabecalho.dadoCabecalho);
+		$scope.thisDadoCabecalho.dadoCabecalho = {};
 	};
 
 	this.editar = function(id){
 		$http.put('/dadoCabecalho/' + id, $scope.dadoCabecalho).success(function(resp){
 			console.log('Editado!');
-			$location.path('/listadadoscabecalho');
+			$scope.thisDadoCabecalho.dadoCabecalho = {};
 		});
 	};
 
@@ -31,31 +37,8 @@ app.controller('DadoCabecalhoController',['$scope', '$rootScope', '$http', '$loc
 			$scope.dadosCabecalho.splice($scope.dadosCabecalho.indexOf(resp), 1);
 		});
 	};
-}]);
-
-app.controller('DadoCabecalhoInsertController', ['$http', '$location', '$routeParams', function($http, $location, $routeParams){
-	this.dadoCabecalho = {};
-	this.dadoCabecalho.formatacao = $routeParams.id;
-	this.criar = function(){
-		$http.post('/dadoCabecalho/create', this.dadoCabecalho).success(function(resp){
-			console.log('Criado!');
-		});
-	};
-}]);
-
-app.controller('DadoCabecalhoEditaController', ['$http', '$routeParams', '$scope', '$location', function($http, $routeParams, $scope, $location){
-	$scope.dadoCabecalho = {};
-
-	$http.get('/dadoCabecalho/'+ $routeParams.id).success(function(resp){
-		$scope.dadoCabecalho = resp;
-	}).error(function(erro){
-		console.log(erro);
-	});
-
-	this.editar = function(id){
-		$http.put('/dadoCabecalho/' + id, $scope.dadoCabecalho).success(function(resp){
-			console.log('Editado!');
-			$location.path('/listadadoscabecalho');
-		});
-	};
+	
+	this.carregar = function (dadoCabecalho){
+		$scope.thisDadoCabecalho.dadoCabecalho = dadoCabecalho;
+	}
 }]);
