0dd0b356cf46d4b73c28c2b8cb95689fc2d280b1
Pedro Goncalves <pedro.goncalves@KPL.LOCAL>
2016-07-29 14:37:55 -0300

    CRUD dos dados do documento
    Adicionadas as funções de CRUD do documento da formatação.

---
+++ assets/js/dadoDocumento.js
@@ -0,0 +1,46 @@
+'use strict';
+
+app.controller('DadoDocumentoController',['$scope', '$rootScope', '$http', '$location', '$routeParams', function($scope, $rootScope, $http, $location, $routeParams){
+	$scope.thisDadoDocumento = this;
+	this.dadoDocumento = {};
+	this.dadoDocumento.formatacao = $routeParams.id;
+	$scope.dadosDocumento = [];
+
+	$http.get('/formatacao/' + $routeParams.id).success(function(resp){
+		$scope.dadosDocumento = resp.dadosDocumento;
+	});
+
+	this.criar = function(){
+		if($scope.thisDadoDocumento.dadoDocumento.id == null){
+			$http.post('/dadoDocumento/create', this.dadoDocumento).success(function(resp){
+				console.log('Criado!');
+				$scope.dadosDocumento.push(resp);
+			});
+		}else{
+			$http.put('/dadoDocumento/' + this.dadoDocumento.id, this.dadoDocumento).success(function(resp){
+				console.log('Editado!');
+			});
+		}
+		$scope.thisDadoDocumento.dadoDocumento = {};
+		this.dadoDocumento.formatacao = $routeParams.id;
+	};
+
+	this.editar = function(id){
+		$http.put('/dadoDocumento/' + id, $scope.dadoDocumento).success(function(resp){
+			console.log('Editado!');
+			$scope.thisDadoDocumento.dadoDocumento = {};
+			this.dadoDocumento.formatacao = $routeParams.id;
+		});
+	};
+
+	this.remover = function(id){
+		$http.delete('/dadoDocumento/' + id).success(function (resp){
+			console.log('apagado');
+			$scope.dadosDocumento.splice($scope.dadosDocumento.indexOf(resp), 1);
+		});
+	};
+	
+	this.carregar = function (dadoDocumento){
+		$scope.thisDadoDocumento.dadoDocumento = dadoDocumento;
+	}
+}]);
